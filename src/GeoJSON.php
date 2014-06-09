bc0b5c17cff7af76815968cb25695d467c91f67b
George Oakling <george@oakling.name>
2014-06-09 15:02:22 +0200

    Create GeoJSON.php

---
+++ src/GeoJSON.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace GeoJSON;
+
+abstract class Feature {
+
+	public $type = "Feature";
+	public $geometry;
+	public $properties;
+	
+	public function __construct() {
+		$this->geometry = array(
+			"type" => $this->getClassName(),
+		);
+	}
+
+	public function addProperty($key, $value) {
+		$this->properties[$key] = $value;
+	}
+	
+	function getClassName() {
+		$classname = get_class($this);
+		if (preg_match('@\\\\([\w]+)$@', $classname, $matches)) {
+			$classname = $matches[1];
+		}
+		return $classname;
+	}
+}
+
+class Point extends Feature {
+	
+	public function setCoordinates($lat, $lng) {
+		$this->geometry["coordinates"][] = (float) $lat;
+		$this->geometry["coordinates"][] = (float) $lng;
+	}
+}
+
+class MultiPoint extends Feature {
+	// todo
+}
+
+class LineString extends Feature {
+	// todo
+}
+
+class MultiLineString extends Feature {
+	// todo
+}
+
+class Polygon extends Feature {
+	// todo
+}
+
+class MultiPolygon extends Feature {
+	// todo
+}
+
+class GeometryCollection extends Feature {
+	// todo
+	// does this support Google Maps?
+}
+
+class FeatureCollection {
+	public $type = "FeatureCollection";
+	public $features = array();
+	
+	public function addFeature(Feature $node) {
+		$this->features[] = $node;
+	}
+	
+	public function __toString() {
+		return json_encode($this);
+	}
+}
+
+
+?>
+
+<xmp>
+
+<?php
+
+$geojson = new FeatureCollection();
+
+for($i=0; $i<5; $i++) {
+	$feat = new Point();
+	$feat->setCoordinates(12.4, 102.6);
+	$feat->addProperty("prop0", "value0");
+	$geojson->addFeature($feat);
+}
+
+echo $geojson;
