b979c41469a4cc320d6982d9e937f669db95c6af
George Oakling <george@oakling.name>
2014-06-13 14:30:22 +0200

    Update fc - add boundingbox setter

--- src/FeatureCollection.php
+++ src/FeatureCollection.php
@@ -7,10 +7,19 @@ use Nette\Object;
 
 class FeatureCollection extends Object {
 	public $type = "FeatureCollection";
+	public $bbox;
 	public $features = array();
 	
 	public function addFeature(Feature $node) {
 		$this->features[] = $node;
+	}
+	
+	public function setBoundingBox($a, $b, $c, $d) {
+		$this->bbox = array();
+		$this->bbox[] = (float) $b;
+		$this->bbox[] = (float) $a;
+		$this->bbox[] = (float) $d;
+		$this->bbox[] = (float) $c;
 	}
 	
 	public function __toString() {
